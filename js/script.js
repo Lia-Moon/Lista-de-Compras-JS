119633aba8579fe1defa405b743b33a23f40c968
Lia-Moon <juliasoaresvieira@hotmail.com>
2023-11-06 22:00:10 -0300

    Criação inicial da funcionalidade javascript para remover itens

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // Adicionar itens na lista de compras
-var addButton = document.querySelector('#botaoAdicionarProduto');
 var itemLista = document.querySelector(".main__content--itens-lista");
+var addButton = document.querySelector("#botaoAdicionarProduto");
 var novoProduto = document.querySelector("#novoProduto");
 
 addButton.addEventListener("click", adicionarProduto);
@@ -20,3 +20,13 @@ novoProduto.addEventListener('keyup', (teclaPressionada) => {
         adicionarProduto();
     }
 })
+
+// Remover itens da lista de compras
+var removeButton = document.querySelector("#botaoRemoverProduto");
+
+removeButton.addEventListener("click", removerProduto);
+
+function removerProduto() {
+    var itemAdicionado = '<li> <i class="fa-regular fa-circle"></i><i class="fa-solid fa-circle"></i>' + value + '</li><button><i class="fa-solid fa-circle-minus"></i></button>';
+        itemLista.innerHTML -= itemAdicionado;
+}
